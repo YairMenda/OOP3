8af8e9e805229e33400dee1a441a4ccc0cec04b4
roy houber <royhou@post.bgu.ac.il>
2023-06-08 15:28:12 +0300

    lets go

--- src/Tiles/Units/Players/Player.java
+++ src/Tiles/Units/Players/Player.java
@@ -1,4 +1,50 @@
 package Tiles.Units.Players;
+import Tiles.Units.Bars.Bar;
+import Tiles.Units.Players.SpecialAbility.SpecialAbility;
 
-public class Player {
+import Tiles.Units.Unit;
+
+public class Player extends Unit {
+
+    private int level;
+    private SpecialAbility specialAB;
+    public Player(SpecialAbility specialAB, String name, int attackPoints, int defensePoints, Bar health, int x, int y, char symbol)
+    {
+        super(name,attackPoints,defensePoints,health,x,y,'@');
+        this.level = 1;
+        this.specialAB = specialAB;
+    }
+
+    public void activateAbility()
+    {
+        this.specialAB.activateAbility();
+    }
+
+    public void gainEXP(int exp)
+    {
+        while(exp > 0)
+        {
+            if(this.getExp() + exp > this.level * 50)
+            {
+                exp = exp - this.level * 50 + this.getExp();
+                levelUP();
+                gainEXP(exp);
+            }
+            this.setExp(this.getExp()+exp);
+        }
+    }
+    public void levelUP()
+    {
+        this.setExp(0);
+        this.level++;
+        this.getHealth().setPool(this.getHealth().getPool() + 10 * this.level);
+        this.getHealth().setCurrent(this.getHealth().getPool());
+        this.setAttackPoints(this.getAttackPoints() + this.level*4);
+        this.setDefensePoints(this.getDefensePoints() + this.level);
+    }
+    
+    public String description()
+    {
+        return super.description() + " Level : " + this.level;
+    }
 }
--- src/Tiles/Units/Unit.java
+++ src/Tiles/Units/Unit.java
@@ -1,4 +1,85 @@
 package Tiles.Units;
+import Tiles.Tile;
+import Tiles.Units.Bars.Bar;
+import jdk.jshell.spi.ExecutionControl;
 
-public class Unit {
+public class Unit extends Tile {
+
+    private String name;
+    private int attackPoints;
+    private int defensePoints;
+    private Bar health;
+    private int exp;
+
+    public Unit(String name,int attackPoints,int defensePoints,Bar health,int x,int y,char symbol)
+    {
+        super(symbol,new Position(x,y));
+        this.name = name;
+        this.attackPoints = attackPoints;
+        this.defensePoints = defensePoints;
+        this.health = health;
+        this.exp = 0;
+    }
+
+    public void attack()
+    {
+
+    }
+
+    public void defense(int damage)
+    {
+
+    }
+
+    public void move()
+    {
+
+    }
+
+    public String description()
+    {
+        System.out.println("Player name: " + this.name + "  AttackPoints: " +
+                this.attackPoints + "  DefensePoints: " + this.defensePoints + "  " +
+                this.health.toString() + "  exp: " + this.exp);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public void setAttackPoints(int attackPoints) {
+        this.attackPoints = attackPoints;
+    }
+
+    public int getDefensePoints() {
+        return defensePoints;
+    }
+
+    public void setDefensePoints(int defensePoints) {
+        this.defensePoints = defensePoints;
+    }
+
+    public Bar getHealth() {
+        return health;
+    }
+
+    public void setHealth(Bar health) {
+        this.health = health;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
 }
